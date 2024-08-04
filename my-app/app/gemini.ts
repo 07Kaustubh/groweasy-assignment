04b524badaea6eef114704e823ae338d3b68174f
07Kaustubh <101243527+07Kaustubh@users.noreply.github.com>
2024-08-04 22:43:45 +0000

    type checking error fixed

--- my-app/app/gemini.ts
+++ my-app/app/gemini.ts
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyDhpQT2ddMYmnQhcEHD7ASLOdEvjommqrU");
 
-async function run(query) {
+async function run(query: string) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
